78da32eeb450f7697e88160b3611fa4ca2d03f7b
Diana Lim <lim.diana@outlook.com>
2021-01-18 16:25:59 -0800

    Created a vue app

---
+++ vision-board/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <h1>2021 Vision Board</h1>
+  
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin:0;
+  padding:0;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+  background-color: #061639;
+  color: #fff;
+  text-align: center;
+}
+</style>
