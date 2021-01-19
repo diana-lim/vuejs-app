ceee98a13fd51d26ded097c5337c414e86ac2714
Diana Lim <lim.diana@outlook.com>
2021-01-18 16:47:50 -0800

    Created a Goals component and used a v-for directive to loop through my goals prop

--- vision-board/src/App.vue
+++ vision-board/src/App.vue
@@ -2,17 +2,39 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <h1>2021 Vision Board</h1>
+    <Goals v-bind:goals="goals" />
   
   </div>
 </template>
 
 <script>
-
+import Goals from './components/Goals';
 
 export default {
   name: 'App',
   components: {
-    
+    Goals
+  },
+  data(){
+    return {
+      goals: [
+        {
+          id: 1,
+          title: "Secure new software engineering role",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "Move to a coastal city",
+          completed: false
+        },
+        {
+          id: 3,
+          title: "Monetize one YouTube channel",
+          completed: false
+        }
+      ]
+    }
   }
 }
 </script>
---
+++ vision-board/src/components/Goals.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>Goals</h1>
+        <div v-bind:key="goal.id" v-for="goal in goals">
+            <h3>{{goal.title}}</h3>
+        </div>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    name: "Goals",
+    props: ["goals"]
+    
+}
+</script>
+
+<style scoped>
+h3 {
+    margin-top: 10px;
+}
+
+</style>
